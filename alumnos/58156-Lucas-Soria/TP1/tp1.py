c992f2a6e32d34e6fa96ac0b10f98ff2758fd09e
Lucas <l.soria@alumno.um.edu.ar>
2020-05-11 20:33:32 -0300

    TP1 Terminado

--- alumnos/58156-Lucas-Soria/TP1/tp1.py
+++ alumnos/58156-Lucas-Soria/TP1/tp1.py
@@ -1,8 +1,17 @@
-#! /usr/bin/python
-import os
+#! /usr/bin/python3
 import argparse
-import multiprocessing
 import array
+import multiprocessing
+import os
+
+
+class FileError(Exception):
+    pass
+
+
+class ArgsError(Exception):
+    pass
+
 
 # Definicion de los argumentos
 parser = argparse.ArgumentParser(description='Tp1 - procesa ppm')
@@ -15,9 +24,15 @@ parser.add_argument('-b', '--blue', type=float, default=1,
 parser.add_argument('-s', '--size', type=int, default=10,
                     help='Bloque de lectura')
 parser.add_argument('-f', '--file', help='Archivo a procesar')
-args = vars(parser.parse_args())
+try:
+    args = vars(parser.parse_args())
+    if args["red"] < 0 or args["green"] < 0 or args["blue"] < 0 or args["size"] < 0:
+        raise ArgsError
+except ArgsError:
+    print("\nERROR\nHubo un problema con los datos ingrsados por argumento\n")
+    exit(-1)
 
-
+# Definicion de las colas para IPC
 queuer = multiprocessing.Queue()
 queueg = multiprocessing.Queue()
 queueb = multiprocessing.Queue()
@@ -70,9 +85,23 @@ def leerdatos(file):
         yield x.strip(" ")
 
 
+# Enviar los datos a los hijos
+def enviar(msn):
+    queuer.put(msn)
+    queueg.put(msn)
+    queueb.put(msn)
+
+
 def main():
     path = os.path.abspath(os.getcwd())
-    file = open("{}/{}".format(path, args["file"]), "rb")
+    try:
+        if "ppm" not in args["file"]:
+            raise FileError
+        file = open("{}/{}".format(path, args["file"]), "rb")
+    except FileError:
+        print("\nERROR\nEl archivo especificado no existe en el directorio" +
+              " o no es un archivo con formato ppm\n")
+        exit(-1)
     red = path + "/r_" + args["file"]
     green = path + "/g_" + args["file"]
     blue = path + "/b_" + args["file"]
@@ -86,7 +115,8 @@ def main():
     hb.start()
     for x in leerdatos(file):
         if a < 3 and a >= 0:
-            cabecera += x
+            a = 0
+            cabecera += x + " "
             for y in range(len(cabecera) - 1):
                 if cabecera[y] + cabecera[y + 1] == "0a":
                     a += 1
@@ -97,29 +127,22 @@ def main():
                     break
         elif a >= 3:
             a = -1
-            queuer.put(cabecera[:fin + 2])
-            queuer.put(cabecera[fin + 3:] + " " + x)
-            queueg.put(cabecera[:fin + 2])
-            queueg.put(cabecera[fin + 3:] + " " + x)
-            queueb.put(cabecera[:fin + 2])
-            queueb.put(cabecera[fin + 3:] + " " + x)
-            continue
+            enviar(cabecera[:fin + 2].strip(" "))
+            if cabecera[fin + 3:]:
+                enviar(cabecera[fin + 3:].strip(" ") + " " + x.strip(" "))
+                continue
         else:
-            queuer.put(x)
-            queueg.put(x)
-            queueb.put(x)
-    queuer.put("Terminamos")
-    queueg.put("Terminamos")
-    queueb.put("Terminamos")
+            enviar(x.strip(" "))
+    enviar("Terminamos")
     os.waitpid(hr.pid, 0)
     os.waitpid(hg.pid, 0)
     os.waitpid(hb.pid, 0)
-    os.system("eog {}/{} &".format(path, args["file"]))
-    print("Se generaron correctamente los 3 filtos")
     hr.join()
     hg.join()
     hb.join()
     file.close()
+    print("\nSe generaron correctamente los 3 filtos\n")
+    os.system("eog {}/{} &".format(path, args["file"]))
 
 
 if __name__ == "__main__":
